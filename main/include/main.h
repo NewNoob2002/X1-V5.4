ae0f4707a2b37c76523035646e5b39573b20b091
NewBoob2002 <guptianci@gmail.com>
2025-02-19 13:43:27 +0800

    main

---
+++ main/include/main.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "_NVS_.h"
+#include "stdio.h"
+#include "string.h"
--- main/main.c
+++ main/main.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+#include "main.h"
 
 void app_main(void)
 {
+    Init_NVS_FS();
+
+    return;
 
 }
